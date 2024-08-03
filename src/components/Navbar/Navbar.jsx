b684b884a3fb9db224554868f84ad1c54aa93e69
ousmane <ousmaneyaou00@gmail.com>
2024-08-03 04:44:22 +0000

    Responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/file.png'
+import iconmenu from '../../assets/menueicon.jpg'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-scroll'
 
@@ -14,10 +15,15 @@ const Navbar = () => {
     })
     }, [])
 
+    const [telMenu, setTelMenu] = useState(false)
+    const toggleMenu = () =>{
+        telMenu ? setTelMenu(false) : setTelMenu(true);
+    }
+
   return (
     <nav className={`container ${collant ? 'couleurDeNav' : ''}`}>
       <img src={logo} alt="" className='logo'/>
-      <ul>
+      <ul className={telMenu ? '' : 'petit-ecran'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='programmes' smooth={true} offset={-260} duration={500}>Programme</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>A propos de nous</Link></li>
@@ -25,7 +31,7 @@ const Navbar = () => {
         <li><Link to='temoignages' smooth={true} offset={-260} duration={500}>Témoignages</Link></li>
         <li><Link to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contactez nous</button></Link></li>
       </ul>
-
+        <img src={iconmenu} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
